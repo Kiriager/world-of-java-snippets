803aa938c99444b19028ea2e0674afee4df5ec5a
Kiriager <kiriager@gmail.com>
2023-09-30 14:32:25 +0300

    add lesson 1

---
+++ lessons/L1-GetStarted/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+        /*
+         * println() виводить текст у консоль
+         * і переносить курсор на наступний рядок  
+         */
+        System.out.println("Hello, World!");
+        
+        //print() робить те саме, але без перенесення курсора
+        System.out.print("My name is ");
+        System.out.print("Honey Badger.");
+
+        System.out.println(5 + 5);
+        System.out.println(5 * 5);
+        System.out.println(5 - 5);
+        System.out.println(10 / 5);
+        System.out.println("10 / 5");
+        System.out.println("My favorite number is " + 5);
+
+        System.out.println("Java");
+        System.out.println("    is");
+        System.out.println("      cool!");
+    }
+}
+
+
