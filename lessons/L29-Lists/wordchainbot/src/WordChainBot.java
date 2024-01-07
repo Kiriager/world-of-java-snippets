d9c3ec61e62ba30e300c3532deabde79cd625131
Kiriager <kiriager@gmail.com>
2024-01-07 22:48:23 +0200

    L29 fix constant

--- lessons/L29-Lists/wordchainbot/src/WordChainBot.java
+++ lessons/L29-Lists/wordchainbot/src/WordChainBot.java
@@ -12,6 +12,7 @@ public class WordChainBot extends TelegramLongPollingBot {
   public static final String WORD_ADDED_TEXT = "Word has been added to the chain.";
   public static final String WORD_CONTAINS_SPACES_ERROR = "Word shoudn't contain spaces. Please try another one.";
   public static final String UNKNOWN_COMMAND_ANSWER_TEXT = "Sorry, I don't know this command... :confused: Try to use one from menu.";
+  public static final String LIST_CLEARED_TEXT = "Word chain has been cleared.";
 
   public static final String CLEAR_COMMAND = "/clear";
   public static final String PRINT_COMMAND = "/print";
@@ -44,7 +45,6 @@ public class WordChainBot extends TelegramLongPollingBot {
     if (itemText.contains(" ")) {
       return WORD_CONTAINS_SPACES_ERROR;
     }
-
     if (wordChain.isEmpty()) {
       wordChain.add(itemText);
       return WORD_ADDED_TEXT;
@@ -65,25 +65,24 @@ public class WordChainBot extends TelegramLongPollingBot {
     return getWordChainLastWord().charAt(getWordChainLastWord().length() - 1); 
   }
 
-
   private String handleCommand(String commandText) {
     if (commandText.equals(PRINT_COMMAND)) {
       return wordChain.toString();
     }
     if (commandText.equals(CLEAR_COMMAND)) {
       wordChain.clear();
-      return "Word chain has been cleared.";
+      return LIST_CLEARED_TEXT;
     }
     return EmojiParser.parseToUnicode(UNKNOWN_COMMAND_ANSWER_TEXT);
   }
 
   @Override
   public String getBotToken() {
-    return "5951902917:AAFwhQPmsKdBOT2gngpmSdUh9nh59R0FF6E";
+    return "BotToken";
   }
 
   @Override
   public String getBotUsername() {
-    return "KiriagerBot";
+    return "BotUsername";
   }
 }
